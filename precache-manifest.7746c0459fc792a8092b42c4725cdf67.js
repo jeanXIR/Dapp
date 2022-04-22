a1cdbe1159cddbdf5e7ec056a6adf001fc39754e
jeanXIR <jean.czyhir@gmail.com>
2022-04-23 01:11:19 +0200

    Updates

---
+++ precache-manifest.7746c0459fc792a8092b42c4725cdf67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43046a5eeac4ce17c385ef896d8723ab",
+    "url": "/jeanXIR/Dapp/index.html"
+  },
+  {
+    "revision": "dfd0f13f5395d4c6b28a",
+    "url": "/jeanXIR/Dapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5723f62d61083f485ea4",
+    "url": "/jeanXIR/Dapp/static/js/2.985976c4.chunk.js"
+  },
+  {
+    "revision": "dfd0f13f5395d4c6b28a",
+    "url": "/jeanXIR/Dapp/static/js/main.bdc1fd86.chunk.js"
+  },
+  {
+    "revision": "820d2385444b0e62b8d8",
+    "url": "/jeanXIR/Dapp/static/js/runtime-main.745b656a.js"
+  }
+]);
